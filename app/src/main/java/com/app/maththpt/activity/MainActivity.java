245980d376e365afdd82e5f06bbe89f88e83d889
HikaruNguyen <Manh.it.it.it@gmail.com>
2017-02-15 11:35:41 +0700

    upgrade lib

--- app/src/main/java/com/app/maththpt/activity/MainActivity.java
+++ app/src/main/java/com/app/maththpt/activity/MainActivity.java
@@ -65,6 +65,8 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        mainViewModel = new MainViewModel(this);
+        activityMainBinding.setViewModelMain(mainViewModel);
         initUI();
         bindData();
         event();
